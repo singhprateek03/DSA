07bbd9ead368b2023bba6118d804cbc0090da48e
Prateek Singh <prateeksingh3102000@gmail.com>
2024-11-05 00:41:52 +0530

    Linear Search using 2D array

---
+++ SearchIn2DArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {23, 4, 1},
+                {18, 12, 3, 9},
+                {78, 99, 34, 56},
+                {18, 12}
+        };
+        int target = 34;
+        int[] ans = search(arr, target); // format of return value is [row][col]
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] search(int[][] arr, int target){
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target){
+                    return new int[]{row, col}; // new int[] -> iss object main humara output store hoga
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
