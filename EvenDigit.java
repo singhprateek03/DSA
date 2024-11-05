98c312bb4e407fcfa5a181d5f308351274d33779
Prateek Singh <prateeksingh3102000@gmail.com>
2024-11-05 22:28:01 +0530

    EvenDigit

--- EvenDigit.java
+++ EvenDigit.java
@@ -1,5 +1,50 @@
 public class EvenDigit {
     public static void main(String[] args) {
-        
+        int[] nums = {12, 345, 2, 6, 7896};
+        System.out.println(+findNumbers(nums)+" Even number present in the array.");
+        System.out.println("Negative value is "+digit(-1000)+" digit");
+        System.out.println(digit(0));
+    }
+
+    static int findNumbers(int[] nums){
+        int count = 0;
+        for (int num: nums){
+            if (even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static boolean even(int num){
+        int numberOfDigits = digit(num);
+     /*
+        if(numberOfDigits % 2 == 0){
+        return true;
+        }
+        return false;
+     */
+        return numberOfDigits % 2 == 0;
+    }
+
+    // count number of digits in a number
+    static int digit(int num){
+
+        if(num == 0){
+            return 1;
+        }
+
+        // For negative number
+        if (num < 0){
+            num *= -1;
+        }
+
+        // for positive number
+        int count = 0;
+        while(num > 0){
+            count++;
+            num /= 10;
+        }
+        return count;
     }
 }
