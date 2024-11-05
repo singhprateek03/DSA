96538fa066c65c48db4cc712a03c2d64b9843eb4
Prateek Singh <prateeksingh3102000@gmail.com>
2024-11-05 20:48:22 +0530

    EvenDigit Question

---
+++ EvenDigit.java
@@ -0,0 +1,5 @@
+public class EvenDigit {
+    public static void main(String[] args) {
+        
+    }
+}
--- SearchIn2DArray.java
+++ SearchIn2DArray.java
@@ -10,9 +10,24 @@ public class SearchIn2DArray {
         };
         int target = 34;
         int[] ans = search(arr, target); // format of return value is [row][col]
-        System.out.println(Arrays.toString(ans));
+//        System.out.println(Arrays.toString(ans));
+        System.out.println(max(arr));
     }
 
+    // For Maximum
+    static int max(int[][] arr){
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] > max){
+                    max = arr[row][col];
+                }
+            }
+        }
+        return max;
+    }
+
+    // To find target value
     static int[] search(int[][] arr, int target){
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr[row].length; col++) {
